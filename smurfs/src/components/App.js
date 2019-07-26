eb224831b12392a37fa27c5f6409a9f984961142
Rory Murray <rory.christian.murray@gmail.com>
2019-07-26 12:16:42 -0400

    added stylings

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -7,6 +7,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <h2>
+          The Smurfs are back in the neighborhood... but will the peace harmony
+          with family and friends last?!?! Join us for this special event to
+          find out!
+        </h2>
         <SmurfList />
         <SmurfForm />
       </div>
