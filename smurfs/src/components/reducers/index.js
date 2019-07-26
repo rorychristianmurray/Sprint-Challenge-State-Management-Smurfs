f452506b83600c3d2111e91d6d833df401e93ad1
Rory Murray <rory.christian.murray@gmail.com>
2019-07-26 11:20:22 -0400

    setup store, reducer, action types

---
+++ smurfs/src/components/reducers/index.js
@@ -0,0 +1,6 @@
+export default (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,5 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import rooterReducer from "./components/reducers";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const AppWithProvider = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+const store = createStore(
+  rooterReducer,
+  composeWithDevTools(applyMiddleware(thunk, logger))
+);
+
+ReactDOM.render(AppWithProvider, document.getElementById("root"));
